2ea20138bc192687f6249865bc15e0522e8c2849
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-30 15:24:29 +0600

    load the coins data

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Coins = () => {
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setCoins(data)
+            })
+    }, [])
     return (
         <div>
-            <h2>Coins page</h2>
+            <h2>Coins: {coins.length}</h2>
         </div>
     );
 };
