f00328fd7953cb0aa4b708c06da388ffc38c9b8c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-30 14:44:56 +0600

    Routing setup for home, coins, contact, about

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
+import About from './components/About/About';
+import Coins from './components/Coins/Coins';
+import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
 import Homepage from './components/Homepage/Homepage';
 
 const App = () => {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Homepage></Homepage>}></Route>
+        <Route path='/coins' element={<Coins></Coins>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Coins/Coins.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Coins = () => {
+    return (
+        <div>
+            <h2>Coins page</h2>
+        </div>
+    );
+};
+
+export default Coins;
